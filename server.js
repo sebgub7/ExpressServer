fd50a935bb3632c7a2fba256385a89cbc29eee96
Sebastian Gubiec <seb.gub@wp.pl>
2022-01-27 20:47:46 +0100

    finished 27.3

--- server.js
+++ server.js
@@ -1,42 +1,41 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
-
-app.use('/user', (req, res, next) => {
-  res.show('forbidden.html');
-  next();
-});
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.show('home.html');
+  res.render('index', { layout: 'dark' });
 });
 
-app.get('/home', (req, res) => {
-  res.show('home.html');
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: 'dark', name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: 'dark' });
 });
 
-app.get('/user', (req, res) => {
-  res.show('about.html');
+app.get('/contact', (req, res) => {
+  res.render('contact', { layout: 'dark' });
+});
+
+app.get('/info', (req, res) => {
+  res.render('info', {layout: 'dark'});
+});
+
+app.get('/history', (req, res) => {
+  res.render('history', { layout: 'dark' });
 });
 
 app.use((req, res) => {
-  res.status(404).show('404.html');
-});
-
+  res.status(404).send('404 not found...');
+})
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
